61ac44d3cdae6780b47a75f33d50445b06625334
SteshaVolkova <stepashechka5@gmail.com>
2026-05-25 20:08:56 +0300

    Setting up threshold values for testing in the config file

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,23 @@
 /** @type {import('jest').Config} */
 const config = {
     verbose: true,
+    // записываем тесты
     collectCoverage: true,
+    // указываем, какие папки проверяем на тесты
     collectCoverageFrom: [
         '<rootDir>/src/**/*.{js,jsx}',
         '!**/node_modules/**',
         '!<rootDir>/src/**/*.mock.*'
-    ]
+    ],
+    // описание, какой процент покрытия тестами должен быть
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: 80,
+        },
+    }
 };
 
 module.exports = config;
--- src/addByX.js
+++ src/addByX.js
@@ -1,3 +1,6 @@
+// намеренное игнорирование написания тестов
+// на эту функцию решает этот комментарий:
+/* istanbul ignore next */
 export function addByX(x) {
     return (num) => x + num;
 }
